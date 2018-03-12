68e7c201f38604d74fbc1cda9fa3f168099dd904
RGu0000 <ryszardgurzeda@gmail.com>
2018-03-12 20:36:43 +0100

    Add validator that passes all tests

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -1,6 +1,20 @@
 class TitleBracketsValidator < ActiveModel::Validator
   def validate(record)
     title = record.title
-    #record.errors[:title] << 'test r' if title == 'r'
+    stack = []
+    brackets = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
+    opening = brackets.keys
+    closing = brackets.values
+    
+    title.each_char.with_index do |char, index|
+      if opening.include?(char)
+        next_char = title[index+1]
+        record.errors[:title] << 'unmatched bracket detected' if closing.include? next_char
+        stack.push << char
+      elsif closing.include?(char)
+        record.errors[:title] << 'unmatched bracket detected' if stack.empty? || (brackets[stack.pop] != char)
+      end
+    end
+    record.errors[:title] << 'unmatched bracket detected' unless stack.empty?
   end
 end
