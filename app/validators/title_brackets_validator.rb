9f9fc7a26c8cc07ac79cf1d1f00f27d602a3c4dd
RGu0000 <ryszardgurzeda@gmail.com>
2018-03-12 21:16:45 +0100

    Refactor title validator

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -1,20 +1,38 @@
 class TitleBracketsValidator < ActiveModel::Validator
   def validate(record)
-    title = record.title
-    stack = []
-    brackets = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
-    opening = brackets.keys
-    closing = brackets.values
-    
-    title.each_char.with_index do |char, index|
-      if opening.include?(char)
-        next_char = title[index+1]
-        record.errors[:title] << 'unmatched bracket detected' if closing.include? next_char
-        stack.push << char
-      elsif closing.include?(char)
-        record.errors[:title] << 'unmatched bracket detected' if stack.empty? || (brackets[stack.pop] != char)
+    initialize_instance_variables(record)
+    validate_params
+    raise_unmatched_error(:title, @record) unless @stack.empty?
+  end
+
+  private
+
+  def initialize_instance_variables(record)
+    @record = record
+    @title = record.title
+    @stack = []
+    initialize_brackets
+  end
+
+  def initialize_brackets
+    @brackets = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
+    @opening = @brackets.keys
+    @closing = @brackets.values
+  end
+
+  def validate_params
+    @title.each_char.with_index do |char, index|
+      if @opening.include?(char)
+        next_char = @title[index + 1]
+        raise_unmatched_error(:title, @record) if @closing.include? next_char
+        @stack.push << char
+      elsif @closing.include?(char)
+        raise_unmatched_error(:title, @record) if @stack.empty? || (@brackets[@stack.pop] != char)
       end
     end
-    record.errors[:title] << 'unmatched bracket detected' unless stack.empty?
+  end
+
+  def raise_unmatched_error(field, record)
+    record.errors[field] << 'unmatched bracket detected'
   end
 end
