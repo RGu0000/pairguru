49a444f93d1474c52f454c3d4428e9948d19e625
RGu0000 <ryszardgurzeda@gmail.com>
2018-03-12 22:22:24 +0100

    Create migration to create Comments Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406223727) do
+ActiveRecord::Schema.define(version: 20180312211808) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
