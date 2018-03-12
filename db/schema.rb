d7839e080e6df0b2325334c440b1063abc9517e8
RGu0000 <ryszardgurzeda@gmail.com>
2018-03-12 22:28:28 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312211808) do
+ActiveRecord::Schema.define(version: 20180312212530) do
 
   create_table "comments", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movie_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
   end
 
   create_table "genres", force: :cascade do |t|
