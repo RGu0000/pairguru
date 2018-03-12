133f4f2a8dac78c9bc50868e47db355932fda033
RGu0000 <ryszardgurzeda@gmail.com>
2018-03-12 23:54:38 +0100

    Validate uniqueness of combination author and movie

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   validates :message, presence: true
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   belongs_to :movie
+  validates_uniqueness_of :movie_id, scope: :author_id
 end
