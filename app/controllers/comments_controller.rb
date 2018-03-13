1e7acb1257ce09e73ae6a6fbbfd6401105823ff2
RGu0000 <ryszardgurzeda@gmail.com>
2018-03-13 23:45:11 +0100

    Minor refactoring

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @movie.comments.create(comment_params)
-    @comment.author_id = current_user.id
-    @comment.movie_id = @movie.id
+    initialize_comment
     if @comment.save
       flash[:notice] = "You have commented on #{@movie.title}. Thank you."
     else
@@ -30,8 +29,8 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:message)
   end
 
-  def check_if_already_rated!
-    movie = Movie.find(params[:movie_id])
-    redirect_to movie unless movie.comments.where(author_id: current_user.id).empty?
+  def initialize_comment
+    @comment.author_id = current_user.id
+    @comment.movie_id = @movie.id
   end
 end
