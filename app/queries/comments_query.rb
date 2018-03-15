485c9207c53c5e9024d6461522b477843cf45856
RGu0000 <ryszardgurzeda@gmail.com>
2018-03-15 22:44:36 +0100

    Delete redundant line in comments query

--- app/queries/comments_query.rb
+++ app/queries/comments_query.rb
@@ -9,7 +9,6 @@ class CommentsQuery
       .where('comments.created_at >= ?', 7.days.ago)
       .group(:name)
       .order('count_all desc')
-      .having('count_all > ?', 0)
       .limit(10)
       .count
       .to_a
