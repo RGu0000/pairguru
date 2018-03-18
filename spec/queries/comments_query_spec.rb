87a889d15561496ac0be3c0537ce09fe8bac8559
RGu0000 <ryszardgurzeda@gmail.com>
2018-03-18 20:20:31 +0100

    Add addition spec to query object

--- spec/queries/comments_query_spec.rb
+++ spec/queries/comments_query_spec.rb
@@ -7,7 +7,16 @@ RSpec.describe CommentsQuery do
     let!(:user) { create(:user, :with_10_comments_last_week) }
     let!(:user2) { create(:user, :with_5_comments_last_week) }
 
-    it { expect(subject.first.comments_count).to eq(10) }
-    it { expect(subject.second.comments_count).to eq(5) }
+    describe 'user has 10 comments' do
+      it { expect(subject.first.comments_count).to eq(10) }
+    end
+
+    describe 'user2 has 5 comments' do
+      it { expect(subject.first.comments_count).to eq(5) }
+    end
+
+    describe 'Query finds 2 users' do
+      it { expect(subject.all.length).to eq(2) }
+    end
   end
 end
