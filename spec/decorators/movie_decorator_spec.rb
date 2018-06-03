9c3b02a1f35f03ec2a3419c32a324a7cc6bf8a73
RGu0000 <ryszardgurzeda@gmail.com>
2018-06-03 18:31:14 +0200

    Movie decorator + rspec

--- spec/decorators/movie_decorator_spec.rb
+++ spec/decorators/movie_decorator_spec.rb
@@ -1,9 +1,8 @@
-require "rails_helper"
-
+require 'rails_helper'
 
 RSpec.describe MovieDecorator do
   describe 'formatted_released_at' do
-    let(:movie) { create(:movie, :created_21_03_2018) }
+    let(:movie) { build_stubbed(:movie, released_at: Date.new(2018,03,21)) }
     it { expect(movie.decorate.formatted_released_at).to eq('21.03.2018') }
   end
 end
